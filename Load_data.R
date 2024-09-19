04cdbc612d87c14c0d0dbc6b4058e11e07ed138e
tpaul4545 <tpaul4545@gmail.com>
2024-09-19 15:59:44 -0400

    Added code that fixes typos in genera for better grouping by genus; added code to apply Morgan's predation types to table and summarise by predation type

--- Load_data.R
+++ Load_data.R
@@ -10,28 +10,86 @@ library(stringr)
 carnivore <- read.csv(file = "G4_hf111-01-prey.csv", header = TRUE, 
                       na.strings = "NA")
 
-unique_species <- unique(carnivore$species) 
-  # 49 unique species represented
-
-
 # first replace spaces in spaces in species names with underscore
 carnivore <- carnivore %>% mutate(species = str_replace(species, " ", "_"))
 head(carnivore)
 str(carnivore)
 
-# determine number of genera represented
-unique_genera <- unique(gsub("_.*","",  x = carnivore$species))
-      # this code is funky - apparently the .* means to neglect everything after
-      # the pattern you're looking for. In this case, I'm telling it to look
-      # for an underscore in each species name, and then remove everything after
-      # that, which leaves us with just our genera!
+unique_species <- unique(carnivore$species) 
+# 49 unique species represented
+
+
+
 
 #let's also just add those genera as their own column to the dataframe so that
 # we can group by that column later on
 carnivore$genus <- gsub("_.*","",  x = carnivore$species)
-head(carnivore)
-tail(carnivore)
+carnivore$genus
 
+# determine number of genera represented
+unique_genera <- unique(gsub("_.*","",  x = carnivore$species))
+# this code is funky - apparently the .* means to neglect everything after
+# the pattern you're looking for. In this case, I'm telling it to look
+# for an underscore in each species name, and then remove everything after
+# that, which leaves us with just our genera!
+
+
+# "Nepenthese" and "Neentes" are both misspellings of the same genus. Can't
+# find any plant with those genera but can pull up the same species name with 
+# the Nepenthes genus
+# "Saracenia" is also evidently a typo! 
+# fix the typos!
+
+carnivore <- carnivore %>% 
+  mutate(genus = replace(genus, genus == "Nepenthese", "Nepenthes"))
+carnivore <- carnivore %>% 
+  mutate(genus = replace(genus, genus == "Neentes", "Nepenthes"))
+carnivore <- carnivore %>% 
+  mutate(genus = replace(genus, genus == "Saracenia", "Sarracenia"))
+
+unique(carnivore$genus)
+str(carnivore)
+
+
+
+
+### Add Predation Method column ################################################
+carnivore$predation_method <- NA
+
+carnivore <- carnivore %>% 
+  mutate(predation_method = replace(predation_method, 
+                                    genus == "Pinguicula" |
+                                    genus == "Drosera", 
+                                    "sticky_traps"))
+
+carnivore <- carnivore %>% 
+  mutate(predation_method = replace(predation_method, 
+                                    genus == "Nepenthes" |
+                                    genus == "Brocchinia" |
+                                    genus == "Sarracenia" |
+                                    genus == "Triphyophyllum", 
+                                    "pitchers"))
+
+carnivore <- carnivore %>% 
+  mutate(predation_method = replace(predation_method, 
+                                    genus == "Utricularia" |
+                                      genus == "Dionaea", 
+                                    "active_trapping"))
+
+carnivore$predation_method
+
+
+#I'll leave this code of this "for-loop" here, but I've discovered using
+# the which function works just fine
+### i <- 4
+### for(i in 1:nrow(carnivore)){
+###   ifelse(test = carnivore$genus[i] == "Pinguicula" | carnivore$genus[i] == "Drosera",
+###         yes = carnivore$predation_method[i] <- "sticky_traps",
+###          no = carnivore$predation[i] <- NA)
+### 
+### }
+
+#"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 str(carnivore)
 head(carnivore)
@@ -123,10 +181,24 @@ filter(carnivore, study == "Williams 1980")
 ###Analzye % Studies and Sum Studies Separately ###############################
 # for each of these, "n" is the number of rows in the original data frame that 
 # were of that genus, in other words, the number of observations on that genus
-
+#### Percentage Data ##########################################################
 carnivore_percentage <- filter(carnivore, units == "percentage")
+carnivore_percentage$study
 
 str(carnivore_percentage)
+
+hist(carnivore_percentage$diptera.sum)
+hist(carnivore_percentage$acarina)
+hist(carnivore_percentage$hymenoptera.not.formicidae)
+hist(carnivore_percentage$thysanoptera)
+hist(carnivore_percentage$homoptera.sum)
+hist(carnivore_percentage$coleoptera)
+hist(carnivore_percentage$araneae)
+
+
+
+dotchart(carnivore_percentage$diptera.sum)
+
 carnivore_percentage_summary <- carnivore_percentage %>% group_by(genus) %>%
   summarise(mean_diptera =mean(diptera.sum),
             mean_acarina = mean(acarina),
@@ -157,10 +229,53 @@ carnivore_percentage_summary <- carnivore_percentage %>% group_by(genus) %>%
             mean_microptera = mean(microptera),
             n_genus = n())
 
+#repeat but by predation type
+carnivore_percentage_summary_pred <- carnivore_percentage %>% group_by(predation_method) %>%
+  summarise(mean_diptera =mean(diptera.sum),
+            mean_acarina = mean(acarina),
+            mean_hymenoptera.not.formicidae = mean(hymenoptera.not.formicidae),
+            mean_thysanoptera = mean(thysanoptera),
+            mean_homoptera.sum = mean(homoptera.sum),
+            mean_coleoptera = mean(coleoptera),
+            mean_araneae = mean(araneae),
+            mean_lepidoptera = mean(lepidoptera),
+            mean_hemiptera = mean(hemiptera),
+            mean_plecoptera = mean(plecoptera),
+            mean_formicidae = mean(formicidae),
+            mean_orthoptera = mean(orthoptera),
+            mean_protura = mean(protura),
+            mean_chilopoda = mean(chilopoda),
+            mean_trichoptera = mean(trichoptera),
+            mean_isopoda = mean(isopoda),
+            mean_neuroptera = mean(neuroptera),
+            mean_diplopoda = mean(diplopoda),
+            mean_odonata = mean(odonata),
+            mean_psocoptera = mean(psocoptera),
+            mean_isoptera = mean(isoptera),
+            mean_copeopoda = mean(copeopoda),
+            mean_cladocera = mean(cladocera),
+            mean_scorpions = mean(scorpions),
+            mean_cyclopoida = mean(cyclopoida),
+            mean_harpaticoida = mean(harpaticoida),
+            mean_microptera = mean(microptera),
+            n_predation_method = n())
 
 
+
+
+#### Individual Counts Data ###################################################
 carnivore_number_indivs <- filter(carnivore, units == "number_of_individuals")
 str(carnivore_number_indivs)
+
+hist(carnivore_number_indivs$diptera.sum)
+hist(carnivore_number_indivs$acarina)
+hist(carnivore_number_indivs$hymenoptera.not.formicidae)
+hist(carnivore_number_indivs$thysanoptera)
+hist(carnivore_number_indivs$homoptera.sum)
+hist(carnivore_number_indivs$coleoptera)
+hist(carnivore_number_indivs$araneae)
+
+
 carnivore_number_indivs_summary <- carnivore_number_indivs %>% group_by(genus) %>%
   summarise(mean_diptera =mean(diptera.sum),
             mean_acarina = mean(acarina),
@@ -191,9 +306,38 @@ carnivore_number_indivs_summary <- carnivore_number_indivs %>% group_by(genus) %
             mean_microptera = mean(microptera),
             n_genus = n())
 
+#repeat but by predation type
+carnivore_number_indivs_summary_pred <- carnivore_number_indivs %>% group_by(predation_method) %>%
+  summarise(mean_diptera =mean(diptera.sum),
+            mean_acarina = mean(acarina),
+            mean_hymenoptera.not.formicidae = mean(hymenoptera.not.formicidae),
+            mean_thysanoptera = mean(thysanoptera),
+            mean_homoptera.sum = mean(homoptera.sum),
+            mean_coleoptera = mean(coleoptera),
+            mean_araneae = mean(araneae),
+            mean_lepidoptera = mean(lepidoptera),
+            mean_hemiptera = mean(hemiptera),
+            mean_plecoptera = mean(plecoptera),
+            mean_formicidae = mean(formicidae),
+            mean_orthoptera = mean(orthoptera),
+            mean_protura = mean(protura),
+            mean_chilopoda = mean(chilopoda),
+            mean_trichoptera = mean(trichoptera),
+            mean_isopoda = mean(isopoda),
+            mean_neuroptera = mean(neuroptera),
+            mean_diplopoda = mean(diplopoda),
+            mean_odonata = mean(odonata),
+            mean_psocoptera = mean(psocoptera),
+            mean_isoptera = mean(isoptera),
+            mean_copeopoda = mean(copeopoda),
+            mean_cladocera = mean(cladocera),
+            mean_scorpions = mean(scorpions),
+            mean_cyclopoida = mean(cyclopoida),
+            mean_harpaticoida = mean(harpaticoida),
+            mean_microptera = mean(microptera),
+            n_predation_method = n())
 
-my_table %>% group_by(Company) %>%
-  summarise(mean_sugar = mean(Sugars)) %>%
-  arrange(mean_sugar)
+
+
 
 
