112a21cb7b0666998759e381ec98f0d997d51640
Dante Centuori <123580822+DanteCnt@users.noreply.github.com>
2024-09-25 23:01:22 -0400

    My attempt at making qq plots, only partially successfull. The nature of our data may make it defiant to the usualy normality tests, and the ones I could do indicate it is definitily not normal.

---
+++ QQCrazy.R
@@ -0,0 +1,40 @@
+## data alterations for qq plot of proportion means
+Alter_CarnPropMean <- as.data.frame(t(carnivore_proportion_means))
+Alter_CarnPropMean <- Alter_CarnPropMean %>% rename_at(1,~"Means")
+qq_proportionmeans <- qqnorm(Alter_CarnPropMean$Means)
+
+## data alterations for qq plot of count means
+Alter_CarnCntMean <- as.data.frame(t(carnivore_count_means))
+Alter_CarnCntMean <- Alter_CarnCntMean %>% rename_at(1, ~"Means")
+qq_countmeans <- qqnorm(Alter_CarnCntMean$Means)
+
+### I want to do invidiual qq plots for the specific counts/proportions grouped by predator methods, but IDK if it's worth it
+### or usefull 
+altCrnCtGrpNoSE<- carnivore_count_grouped %>% select(-(SE_diptera:n_predation_method))
+CrnCtGrpNoSE<- as.data.frame(t(altCrnCtGrpNoSE))
+CrnCtGrpNoSE<- CrnCtGrpNoSE %>% `colnames<-`(c("Active_trapping", "Pitchers", "Sticky_Traps"))
+CrnCtGrpNoSE<- CrnCtGrpNoSE %>% filter(!row_number() %in% c(1))
+CrnCtGrpNoSE<- CrnCtGrpNoSe %>% mutate(Active_trapping = gsub(" ","", Active_trapping))
+CrnCtGrpNoSE<- CrnCtGrpNoSe %>% mutate(Pitchers = gsub(" ","", Pitchers))
+CrnCtGrpNoSE<- CrnCtGrpNoSe %>% mutate(Sticky_Traps = gsub(" ","", Sticky_Traps))
+
+hist(altCrnCtGrpNoSE$active_trapping)
+
+qq_CtActive <- qqnorm(CrnCtGrpNoSE$Active_trapping)
+qq_CtPitcher <- qqnorm(CrnCtGrpNoSE$Pitchers)
+qq_CtSticky <- qqnorm(CrnCtGrpNoSE$Sticky_Traps)
+
+
+
+CrnPrpGrpNoSE<- carnivore_proportion_grouped %>% select(-(SE_diptera:n_predation_method))
+CrnPrpGrpNoSE<- as.data.frame(t(CrnPrpGrpNoSE))
+CrnPrpGrpNoSE<- CrnPrpGrpNoSE %>% `colnames<-`(c("Active_trapping", "Pitchers", "Sticky_Traps"))
+CrnPrpGrpNoSE<- CrnPrpGrpNoSE %>% filter(!row_number() %in% c(1))
+CrnPrpGrpNoSE<- CrnPrpGrpNoSE %>% mutate(Active_trapping = gsub(" ","", Active_trapping))
+CrnPrpGrpNoSE<- CrnPrpGrpNoSE %>% mutate(Pitchers = gsub(" ","", Pitchers))
+CrnPrpGrpNoSE<- CrnPrpGrpNoSE %>% mutate(Sticky_Traps = gsub(" ","", Sticky_Traps))
+
+qq_PrpActive <- qqnorm(CrnPrpGrpNoSE$Active_trapping)
+qq_PrpPitcher <- qqnorm(CrnPrpGrpNoSE$Pitchers)
+qq_PrpSticky <- qqnorm(CrnPrpGrpNoSE$Sticky_Traps)
+
