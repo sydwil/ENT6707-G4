791a36f22a6e68ed0269bc245fb6a6915746a51d
sydwil <wilson.3509@buckeyemail.osu.edu>
2024-10-20 12:55:00 -0400

    Not much has changed, I just pushed my most recent version

--- 10-18-model-mess-SNW.R
+++ 10-18-model-mess-SNW.R
@@ -1,12 +1,15 @@
 ##I used the load data r file to get to this point, so all function names will be from that document
-carnivore_not_percentage
+carnivore_number_indivs
 library(ggplot2)
 library(car)
 ##I'm just checking to see if I can set up a model with predation sum as a predictor of diptera total
-testmod <- lm(diptera.sum~predation_method, data = carnivore_not_percentage)
-anova(testmod)
+testmod <- lm(diptera.sum~predation_method, data = carnivore_number_indivs)
+Anova(testmod)
 summary(testmod)
-testmod2 <- lm(diptera.sum ~ predation_method + species, data = carnivore_not_percentage)
-summary(testmod2)
-Anova(testmod2)
-## I haven't done much today, got sidetracked but I'll add to this!!!
+##Basically what it looks like is the diptera don't show any difference in predation method between trap types, but this model also accounts for very little variation
+
+##I'm not feeling very confident in my ability to fit a complicated model, BUT!
+# Compute percentages
+pitchers <- subset(carnivore_number_indivs, predation_method == "pitchers")
+#summary of the columns instead of manually adding them, but then the output isn't one we can use in a graph easily
+pitcher_sum <- pitchers %>% bind_rows(summarise_all(., ~if(is.numeric(.)) sum(.) else "Total"))
