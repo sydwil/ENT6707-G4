5ebac0afde5c48a35d161ecc5b436f20e382e01b
sydwil <wilson.3509@buckeyemail.osu.edu>
2024-10-22 22:56:48 -0400

    woops here's the final update of the night

--- speciesrichness.R
+++ speciesrichness.R
@@ -1,8 +1,8 @@
 ##Syd's attempts at order/group richness because we don't have species
 ##also! I softwrap long lines in the "Code" options, sorry if that's a struggle to read
-##used Load_data.R to give myself a shortcut
-carnivore <- read.csv("/Users/schoolpersona/Desktop/ENT6707/ENT6707_DataAnalysis/Group project folder/ENT6707-G4/G4_hf111-01-prey.csv", header = TRUE, 
-                      na.strings = "NA")
+
+##used Load_data.R
+
 ##working off a tutorial from --> https://rpubs.com/mbh038/719881
 ##this data bit is loaded from Analysis.R
 carnie_count <-  carnivore %>% filter(units == "number_of_individuals")
@@ -19,10 +19,23 @@ testing <- carnie_count %>%
 glimpse(testing)
 ##I miss my big PC screen so much.
 ps<-table(testing$arthropod_group,carnie_count$predation_method)
-##this method won't work, I guess we have more of one species than another, BUT if I do it for each individual 
-psr<-colSums(ps)
-##so we could technically do this for every order, but obviously it would be much nicer if we could do it faster?
-library(tidyverse)
-install.packages("lme4")
-yes
-install.packages("gfortran")
+##this method won't work, I guess we have more of one species than another, BUT if I do it for each individual trap type, that's 3 graphs we can either put next to each other or easily compare!
+
+sticky <- subset(testing, predation_method == "sticky_traps")
+stickytab <- table(sticky$arthropod_group, sticky$predation_method)
+stickysum<-colSums(stickytab)
+print(stickysum)
+##something is funky with this...
+
+library(plotrix)
+##https://www.flutterbys.com.au/stats/tut/tut13.2.html
+#apply(sticky[,-1]>0,1,sum) this is a mess, so I'm going to remove some columns
+sticky2 = subset(sticky, select = -c(genus , species, units, study))
+apply(sticky2[,-1]>0,1,sum)
+##this method isn't going to work with how the columns are laid out unfortunately
+##so let's try again with a different method
+library(plyr)
+ddply (sticky,~ sites ,function(arthropod_group) {
+  +   data.frame(RICHNESS=sum(arthropod_group[-1]>0))})
+##getting closer, but I fear I am still not picking the correct variables.
+#the good thing is I don't think this code is too awful, again the way our data frame is set up makes everything way more complicated
